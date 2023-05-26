b4c89d1232e901795e7b82c96b072755f732f31f
FalguniSaini <falgunisaini675@gmail.com>
2023-05-26 22:54:00 +0530

    code

--- Paytm/config.js
+++ Paytm/config.js
@@ -1,6 +1,6 @@
 var PaytmConfig = {
-  mid: "tmRoBh90182073607271",
-  key: "fAPNNg3!h3K60ZsM",
+  mid: "DIY12386817555501617",
+  key: "bKMfNxPPf_QdZppa",
   website: "WEBSTAGING",
 };
 module.exports.PaytmConfig = PaytmConfig;
